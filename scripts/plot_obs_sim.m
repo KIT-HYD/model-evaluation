72c043456884dd3fb9bef813e88224f3587e3e3d
Elnaz <elnaz.azmi@kit.edu>
2020-01-10 14:41:11 +0100

    some visualization changes!

--- scripts/plot_obs_sim.m
+++ scripts/plot_obs_sim.m
@@ -13,14 +13,14 @@ load model_06_out
 
 figure('units','normalized','outerposition',[0 0 0.8 1])
 hold on
-plot(DatumZeit(29401:29617),q_host(29401:29617),'LineStyle','--','LineWidth',4,'color',rgb('steelblue'));
-plot(DatumZeit(29401:29617),output_00(29401:29617),'LineWidth',1,'color',rgb('teal'));
-plot(DatumZeit(29401:29617),output_01(29401:29617),'LineWidth',1,'color',rgb('red'));
-plot(DatumZeit(29401:29617),output_02(29401:29617),'LineWidth',1,'color',rgb('darkviolet'));
-plot(DatumZeit(29401:29617),output_03(29401:29617),'LineWidth',1,'color',rgb('darkorange'));
-plot(DatumZeit(29401:29617),output_04(29401:29617),'LineWidth',1,'color',rgb('darkolivegreen'));
-plot(DatumZeit(29401:29617),output_05(29401:29617),'LineWidth',1,'color',rgb('sienna'));
-plot(DatumZeit(29401:29617),output_06(29401:29617),'LineWidth',1,'color',rgb('green'));
+plot(DatumZeit(29401:29617),q_host(29401:29617),'LineWidth',8,'color',[cp{1,:}]);
+plot(DatumZeit(29401:29617),output_00(29401:29617),'LineWidth',4,'color',[cp{3,:}]);
+plot(DatumZeit(29401:29617),output_01(29401:29617),'LineStyle','--','LineWidth',4,'color',[cp{11,:}]);
+plot(DatumZeit(29401:29617),output_02(29401:29617),'LineWidth',8,'color',[cp{4,:}]);
+plot(DatumZeit(29401:29617),output_03(29401:29617),'LineStyle','--','LineWidth',6,'color',[cp{5,:}]);
+plot(DatumZeit(29401:29617),output_04(29401:29617),'LineWidth',4,'color',[cp{9,:}]);
+plot(DatumZeit(29401:29617),output_05(29401:29617),'LineWidth',4,'color',[cp{2,:}]);
+plot(DatumZeit(29401:29617),output_06(29401:29617),'LineStyle',':','LineWidth',4,'color',[cp{12,:}]);
 
 h = legend('Observation', 'Model-00', 'Model-01', 'Model-02', 'Model-03', 'Model-04', 'Model-05', 'Model-06');
 set(h,'Location','northeast');
@@ -36,4 +36,4 @@ hold off
 
 curr_path = pwd;
 out_path = [curr_path(1:find(pwd == '/', 1, 'last')) 'output/'];
-print ([out_path 'fig_obs_sim'], '-dpng', '-r600');
+print ([out_path 'fig_obs_sim'], '-dsvg', '-r600');
--- scripts/plot_results.m
+++ scripts/plot_results.m
@@ -31,21 +31,21 @@ labels = {'M-00' 'M-01' 'M-02' 'M-03' 'M-04' 'M-05' 'M-06'};
 figure('units','normalized','outerposition',[0 0 0.8 1])
 % axes('Position',[0.07 0.09 0.52 0.8]); % [left bottom width height]
 hold on
-scatter(h_cond(1),bit_invested_per_ts(1),80, 'v', 'filled', 'MarkerFaceColor',rgb('teal'));
-scatter(h_cond(2),bit_invested_per_ts(2),80, '>', 'filled', 'MarkerFaceColor',rgb('red'));
-scatter(h_cond(3),bit_invested_per_ts(3),80, 'v', 'filled', 'MarkerFaceColor',rgb('darkviolet'));
-scatter(h_cond(4),bit_invested_per_ts(4),80, '>', 'filled', 'MarkerFaceColor',rgb('darkorange'));
-scatter(h_cond(5),bit_invested_per_ts(5),80, '^', 'filled', 'MarkerFaceColor',rgb('darkolivegreen'));
-scatter(h_cond(6),bit_invested_per_ts(6),80, '<', 'filled', 'MarkerFaceColor',rgb('sienna'));
-scatter(h_cond(7),bit_invested_per_ts(7),80, '^', 'filled', 'MarkerFaceColor',rgb('green'));
+scatter(h_cond(1),bit_invested_per_ts(1),80, 'v', 'filled', 'MarkerFaceColor',[cp{3,:}]);
+scatter(h_cond(2),bit_invested_per_ts(2),80, '>', 'filled', 'MarkerFaceColor',[cp{11,:}]);
+scatter(h_cond(3),bit_invested_per_ts(3),80, 'v', 'filled', 'MarkerFaceColor',[cp{4,:}]);
+scatter(h_cond(4),bit_invested_per_ts(4),80, '>', 'filled', 'MarkerFaceColor',[cp{5,:}]);
+scatter(h_cond(5),bit_invested_per_ts(5),80, '^', 'filled', 'MarkerFaceColor',[cp{9,:}]);
+scatter(h_cond(6),bit_invested_per_ts(6),80, '<', 'filled', 'MarkerFaceColor',[cp{2,:}]);
+scatter(h_cond(7),bit_invested_per_ts(7),80, '^', 'filled', 'MarkerFaceColor',[cp{12,:}]);
 
-labelpoints(h_cond(1),bit_invested_per_ts(1),labels(1), 'SE', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', rgb('teal'));
-labelpoints(h_cond(2),bit_invested_per_ts(2),labels(2), 'E', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', rgb('red'));
-labelpoints(h_cond(3),bit_invested_per_ts(3),labels(3), 'S', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', rgb('darkviolet'));
-labelpoints(h_cond(4),bit_invested_per_ts(4),labels(4), 'E', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', rgb('darkorange'));
-labelpoints(h_cond(5),bit_invested_per_ts(5),labels(5), 'NE', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', rgb('darkolivegreen'));
-labelpoints(h_cond(6),bit_invested_per_ts(6),labels(6), 'W', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', rgb('sienna'));
-labelpoints(h_cond(7),bit_invested_per_ts(7),labels(7), 'N', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', rgb('green'));
+labelpoints(h_cond(1),bit_invested_per_ts(1),labels(1), 'SE', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', [cp{3,:}]);
+labelpoints(h_cond(2),bit_invested_per_ts(2),labels(2), 'E', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', [cp{11,:}]);
+labelpoints(h_cond(3),bit_invested_per_ts(3),labels(3), 'S', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', [cp{4,:}]);
+labelpoints(h_cond(4),bit_invested_per_ts(4),labels(4), 'E', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', [cp{5,:}]);
+labelpoints(h_cond(5),bit_invested_per_ts(5),labels(5), 'NE', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', [cp{9,:}]);
+labelpoints(h_cond(6),bit_invested_per_ts(6),labels(6), 'W', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', [cp{2,:}]);
+labelpoints(h_cond(7),bit_invested_per_ts(7),labels(7), 'N', 0.2, 'FontSize',32,'FontWeight','bold', 'Color', [cp{12,:}]);
 
 X = [log2(151), log2(151)];
 Y = [0, 4000];
@@ -65,26 +65,26 @@ hold off
 
 curr_path = pwd;
 out_path = [curr_path(1:find(pwd == '/', 1, 'last')) 'output/'];
-print ([out_path 'fig_bit_by_bit_'], '-dpng', '-r600');
+print ([out_path 'fig_bit_by_bit_'], '-dsvg', '-r600');
 
 % create zoom-in plot
 figure('units','normalized','outerposition',[0 0 0.6 0.8])
 % axes('Position',[.68 .3 .3 .4]); % [left bottom width height]
 box on;
 hold on;
-scatter(h_cond(1),bit_invested_per_ts(1),80, 'v', 'filled', 'MarkerFaceColor',rgb('teal'));
-scatter(h_cond(3),bit_invested_per_ts(3),80, 'v', 'filled', 'MarkerFaceColor',rgb('darkviolet'));
-scatter(h_cond(4),bit_invested_per_ts(4),80, '>', 'filled', 'MarkerFaceColor',rgb('darkorange'));
-scatter(h_cond(5),bit_invested_per_ts(5),80, '^', 'filled', 'MarkerFaceColor',rgb('darkolivegreen'));
-scatter(h_cond(6),bit_invested_per_ts(6),80, '<', 'filled', 'MarkerFaceColor',rgb('sienna'));
-scatter(h_cond(7),bit_invested_per_ts(7),80, '^', 'filled', 'MarkerFaceColor',rgb('green'));
+scatter(h_cond(1),bit_invested_per_ts(1),80, 'v', 'filled', 'MarkerFaceColor',[cp{3,:}]);
+scatter(h_cond(3),bit_invested_per_ts(3),80, 'v', 'filled', 'MarkerFaceColor',[cp{4,:}]);
+%scatter(h_cond(4),bit_invested_per_ts(4),80, '>', 'filled', 'MarkerFaceColor',[cp{5,:}]);
+scatter(h_cond(5),bit_invested_per_ts(5),80, '^', 'filled', 'MarkerFaceColor',[cp{9,:}]);
+scatter(h_cond(6),bit_invested_per_ts(6),80, '<', 'filled', 'MarkerFaceColor',[cp{2,:}]);
+scatter(h_cond(7),bit_invested_per_ts(7),80, '^', 'filled', 'MarkerFaceColor',[cp{12,:}]);
 
-labelpoints(h_cond(1),bit_invested_per_ts(1),labels(1), 'SE', 0.01, 'FontSize',32,'FontWeight','bold', 'Color', rgb('teal'));
-labelpoints(h_cond(3),bit_invested_per_ts(3),labels(3), 'S', 0.01, 'FontSize',32,'FontWeight','bold', 'Color', rgb('darkviolet'));
-labelpoints(h_cond(4),bit_invested_per_ts(4),labels(4), 'E', 0.01, 'FontSize',32,'FontWeight','bold', 'Color', rgb('darkorange'));
-labelpoints(h_cond(5),bit_invested_per_ts(5),labels(5), 'NE', 0.01, 'FontSize',32,'FontWeight','bold', 'Color', rgb('darkolivegreen'));
-labelpoints(h_cond(6),bit_invested_per_ts(6),labels(6), 'W', 0.3, 'FontSize',32,'FontWeight','bold', 'Color', rgb('sienna'));
-labelpoints(h_cond(7),bit_invested_per_ts(7),labels(7), 'N', 0.01, 'FontSize',32,'FontWeight','bold', 'Color', rgb('green'));
+labelpoints(h_cond(1),bit_invested_per_ts(1),labels(1), 'SE', 0.01, 'FontSize',32,'FontWeight','bold', 'Color', [cp{3,:}]);
+labelpoints(h_cond(3),bit_invested_per_ts(3),labels(3), 'S', 0.01, 'FontSize',32,'FontWeight','bold', 'Color', [cp{4,:}]);
+%labelpoints(h_cond(4),bit_invested_per_ts(4),labels(4), 'E', 0.01, 'FontSize',32,'FontWeight','bold', 'Color', [cp{5,:}]);
+labelpoints(h_cond(5),bit_invested_per_ts(5),labels(5), 'NE', 0.01, 'FontSize',32,'FontWeight','bold', 'Color', [cp{9,:}]);
+labelpoints(h_cond(6),bit_invested_per_ts(6),labels(6), 'W', 0.3, 'FontSize',32,'FontWeight','bold', 'Color', [cp{2,:}]);
+labelpoints(h_cond(7),bit_invested_per_ts(7),labels(7), 'N', 0.01, 'FontSize',32,'FontWeight','bold', 'Color', [cp{12,:}]);
 
 xlim([2.5 4]);
 ylim([700 780]);
@@ -94,4 +94,4 @@ ylabel('Data invested (bit)');
 set(gca, 'FontName', 'Liberation Sans','FontSize',32,'FontWeight','bold');
 set(gca,'LooseInset',get(gca,'TightInset')); % erase unnnecesary outside whitespace
 
-print ([out_path 'fig_bit_by_bit_sub_'], '-dpng', '-r600');
+print ([out_path 'fig_bit_by_bit_sub_'], '-dsvg', '-r600');
