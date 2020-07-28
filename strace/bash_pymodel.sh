005668bdb94254682eed02e47671cb11be6b0dd2
Elnaz <elnaz.azmi@kit.edu>
2020-07-28 11:15:08 +0200

    updated outputs!

--- strace/bash_pymodel.sh
+++ strace/bash_pymodel.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+cd scripts
+
 for i in {0..8}
 do
-    if [ ! -d "logs/logmodel_0$i" ]
+    if [ ! -d "../logs/logmodel_0$i" ]
     then
         echo "directory doesn't exist. creating now..."
-        mkdir -p logs/logmodel_0$i
+        mkdir -p ../logs/logmodel_0$i
     fi
 done
 
@@ -15,8 +17,8 @@ for i in {1..100}
 do
     for j in {0..8}
     do
-        strace -o logs/logmodel_0$j/pymodel_0${j}_$i.log -e trace=$tr python3.6 scripts/pymodel_0$j.py
-        rm -rf data/model_0${j}_out.csv
+        strace -o ../logs/logmodel_0$j/pymodel_0${j}_$i.log -e trace=$tr python3.6 pymodel_0$j.py
+        rm -rf ../data/model_0${j}_out.csv
         echo 3 > /proc/sys/vm/drop_caches
         date +"%c| pymodel_0$j is done & pagecache, dentries and inodes is freed."
     done
